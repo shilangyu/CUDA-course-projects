bfc78a0ba635fc62d899b82de7c7e88f851a81d4
shilangyu <xmarcinmarcin@gmail.com>
2022-01-01 18:21:17 +0100

    Add sum reduction

--- k_means/device.cu
+++ k_means/device.cu
@@ -30,19 +30,21 @@ __device__ static inline auto nearest_centroid(
     const std::size_t object_index) -> std::size_t {
   auto min_dist         = distance<n>(objects, centroids, N, k, object_index, 0);
   std::size_t member_of = 0;
-  for (auto j = 1; j < k; j++) {
-    auto dist = distance<n>(objects, centroids, N, k, object_index, j);
+  for (auto i = 1; i < k; i++) {
+    auto dist = distance<n>(objects, centroids, N, k, object_index, i);
 
     if (dist < min_dist) {
       min_dist  = dist;
-      member_of = j;
+      member_of = i;
     }
   }
 
   return member_of;
 }
 
-template <std::size_t n>
+/// Stores in `memberships` indexes of the centroid an object is closest to
+/// Additionally stores in `changed_counter` the amount of changed memberships (thus given `memberships` should contain previous memberships)
+template <std::size_t n, std::size_t block_size>
 __global__ auto get_memberships(
     const float *objects,
     const float *centroids,
@@ -50,11 +52,13 @@ __global__ auto get_memberships(
     const std::size_t k,
     std::size_t *memberships,
     int *changed_counter) -> void {
-  // array of 0/1 flags saying whether a membership has changed. It is later reduced to a sum (to avoid atomic adds)
-  extern __shared__ std::uint8_t changed[];
+  static_assert((block_size & (block_size - 1)) == 0, "block_size has to be a power of two");
 
-  const auto index = blockIdx.x * blockDim.x + threadIdx.x;
-  const auto local = blockIdx.x;
+  // array of 0/1 flags saying whether a membership has changed. It is later reduced to a sum (to avoid atomic adds)
+  __shared__ std::uint8_t changed[block_size];
+
+  const auto index = blockIdx.x * block_size + threadIdx.x;
+  const auto local = threadIdx.x;
 
   changed[local] = 0;
 
@@ -63,9 +67,39 @@ __global__ auto get_memberships(
 
     changed[local]     = member_of != memberships[index];
     memberships[index] = member_of;
+  }
+  __syncthreads();
 
-    // TODO: reduce the sum from `changed`
-    atomicAdd(changed_counter, changed[local]);
+  // reduce `changed` to a sum
+  if constexpr (block_size >= 1024) {
+    if (local < 512) changed[local] += changed[local + 512];
+    __syncthreads();
+  }
+  if constexpr (block_size >= 512) {
+    if (local < 256) changed[local] += changed[local + 256];
+    __syncthreads();
+  }
+  if constexpr (block_size >= 256) {
+    if (local < 128) changed[local] += changed[local + 128];
+    __syncthreads();
+  }
+  if constexpr (block_size >= 128) {
+    if (local < 64) changed[local] += changed[local + 64];
+    __syncthreads();
+  }
+  // no need for __syncthreads since we are in a single warp
+  if (local < 32) {
+    if constexpr (block_size >= 64) changed[local] += changed[local + 32];
+    if constexpr (block_size >= 32) changed[local] += changed[local + 16];
+    if constexpr (block_size >= 16) changed[local] += changed[local + 8];
+    if constexpr (block_size >= 8) changed[local] += changed[local + 4];
+    if constexpr (block_size >= 4) changed[local] += changed[local + 2];
+    if constexpr (block_size >= 2) changed[local] += changed[local + 1];
+  }
+
+  if (local == 0) {
+    // TODO: consider performing further reduction instead of an atomic add
+    atomicAdd(changed_counter, changed[0]);
   }
 }
 
@@ -93,12 +127,11 @@ auto d_k_means(DeviceData data, const std::size_t N, const std::size_t k, std::s
   for (auto iter = 0; iter < max_iters && *changed != 0; iter++) {
     *changed = 0;
 
-    dim3 thread_dim(1024);
+    constexpr dim3 thread_dim(1024);
 
-    get_memberships<n><<<
+    get_memberships<n, thread_dim.x><<<
         N / thread_dim.x + 1,
-        thread_dim,
-        thread_dim.x * sizeof(std::uint8_t)>>>(data.objects, data.centroids, N, k, memberships, changed);
+        thread_dim>>>(data.objects, data.centroids, N, k, memberships, changed);
     cudaDeviceSynchronize();
 
     // TODO: move the next bit of code to CUDA
